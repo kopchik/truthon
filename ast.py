1df874e06a80d1b4108e95ca043a63f60481f336
Alexandre Kandalintsev <spam@messir.net>
2013-08-21 21:53:03 +0200

    ast.py: some preliminary version

--- ast.py
+++ ast.py
@@ -2,54 +2,58 @@
 
 from collections import defaultdict
 
+def factory():
+  class Op:
+    lbp = None
+    rbp = None
+    sym = None
+    def nud(self, expr):
+      """ null denotation (starting token) """
+      raise Exception("this token cannot start expr")
 
-class Op:
-  lbp = None
-  rbp = None
-  sym = None
-  def nud(self):
-    """ null denotation (starting token) """
-    raise Exception("this token cannot start expr")
-
-  def led(self, left):
-    """ left denotation """
-    raise Exception("this token cannot be in the middle of expr")
-  def __repr__(self):
-    cls = self.__class__.__name__
-    return "%s(%s<'%s'>%s)" % (cls, self.lbp, self.sym, self.rbp)
-symap = defaultdict(Op)
+    def led(self, left, expr):
+      """ left denotation """
+      raise Exception("this token cannot be in the middle of expr")
+    def __repr__(self):
+      cls = self.__class__.__name__
+      return "%s(%s<'%s'>%s)" % (cls, self.lbp, self.sym, self.rbp)
+  return Op
+symap = defaultdict(factory)
 
 
 class prefix:
   def __init__(self, sym, rbp):
     self.sym = sym
     self.rbp = rbp
+
   def __call__(self, cls):
     op = symap[self.sym]
     def nud(self, expr):
+      print(self)
       return cls(expr(self.rbp))
     op.nud = nud
     op.rbp = self.rbp
     op.sym = self.sym
+    return cls
 
 
 class infix:
   def __init__(self, sym, lbp):
     self.sym = sym
     self.lbp = lbp
+
   def __call__(self, cls):
     op = symap[self.sym]
-    def led(self, expr, left):
+    def led(self, left, expr):
       return cls(left, expr(self.lbp))
-    op.sym = self.sym
     op.led = led
+    op.sym = self.sym
     op.lbp = self.lbp
-    print(op, led, op.led)
+    return cls
 
 
 class Value:
   lbp = 0
-  rbp = 0
   def __init__(self, value):
     self.value = value
   def nud(self, expr):
@@ -61,6 +65,8 @@ class Value:
 
 class END:
   lbp = 0
+  def __repr__(self):
+    return "END"
 
 #--
 class Binary:
@@ -72,11 +78,20 @@ class Binary:
     cls = self.__class__.__name__
     return "(%s %s %s)" % (cls, self.left, self.right)
 
+class Unary:
+  def __init__(self, value):
+    self.value = value
+  def __repr__(self):
+    cls = self.__class__.__name__
+    return "(%s %s)" % (cls, self.value)
 
-@prefix('-', 10)
-class Minus:
-  def __init__(self, expr):
-    self.expr = expr
+@prefix('-', 100)
+class Minus(Unary):
+  pass
+
+@prefix('+', 100)
+class Plus(Unary):
+  pass
 
 @infix('+', 10)
 class Add(Binary):
@@ -105,13 +120,24 @@ class Expr:
     left = cur.nud(expr)
     while rbp < nxt.lbp:
       cur, nxt = self.shift()
-      left = cur.led(cur, expr, left)
+      print(cur, nxt)
+      left = cur.led(left, expr)
     return left
 
+def tokenizer(s):
+  tokens = s.split()
+  for t in tokens:
+    if t in symap:
+      yield symap[t]()
+    else:
+      yield Value(t)
+  yield END()
 
 def main():
-  tokens = "1 - 2".split()
-  tokens = symap['-'], Value(1), symap['+'], Value(2), END()
+  tokens = tokenizer("1 + 2 + 3")
+  # print(list(tokens))
+  # tokens =  symap['-'](), Value(1), symap['+'](), Value(2), END()
+  # tokens = symap['-'](), Value(1), END()
   expr = Expr(tokens)
   print(expr.expr())
 
