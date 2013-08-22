dd446c6f641006857b609b5112a0f1d41eaa0175
Alexandre Kandalintsev <spam@messir.net>
2013-08-22 16:24:55 +0200

    pratt.py: some debug stuff

--- pratt.py
+++ pratt.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
-from useful.log import Log
-import sys
-sys.setrecursionlimit(30)
+# import sys; sys.setrecursionlimit(30)
+from useful.mstring import s
 
 class Op:
   lbp = None  # left binding power (left-to-right precedence)
   rbp = None  # right binding power
   def nud(self):
-    """ null denotation (starting token)"""
+    """ null denotation (starting token) """
     raise Exception("this token cannot start expr")
 
   def led(self, left):
@@ -21,28 +20,54 @@ class VALUE(Op):
     self.value = value
 
   def nud(self):
-    right = self.value
-    return right
+    return self
 
   def __repr__(self):
-    return "%s(%s)" % (self.__class__.__name__, self.value)
+    cls = self.__class__.__name__
+    return "%s" % (self.value)
+    # return "(%s %s)" % (cls, self.value)
 
+class Binary:
+  def __init__(self, left, right):
+    self.left = left
+    self.right = right
+  def __repr__(self):
+    cls = self.__class__.__name__
+    return s("(${cls} ${self.left} ${self.right})")
+
+class Unary:
+  def __init__(self, value):
+    self.value = value
+  def __repr__(self):
+    cls = self.__class__.__name__
+    return "(%s %s)" % (cls, self.value)
+
+
+class ADD(Binary): pass
+class SUB(Binary): pass
+class POS(Unary): pass
+class NEG(Unary): pass
 
 class PLUS(Op):
   lbp = 10
   rbp = 100
   def nud(self):
-    return expr(self.rbp)
+    return POS(expr(self.rbp))
   def led(self, left):
-    return left + expr(self.lbp)
+    right = expr(self.lbp)
+    print("was in led", left, right)
+    return ADD(left, right)
+  def __repr__(self):
+    return "PLUS"
 
 class MINUS(Op):
   lbp = 10
   rbp = 100
   def nud(self):
-    return -expr(self.rbp)
+    return NEG(expr(self.rbp))
   def led(self, left):
-    return left - expr(self.lbp)
+    return SUB(left, expr(self.lbp))
+
 
 class MULT(Op):
   lbp = 20
@@ -58,6 +83,8 @@ class POWER(Op):
 
 class END:
   lbp = 0
+  def __repr__(self):
+    return "END"
 
 
 def shift():
@@ -84,5 +111,7 @@ def parse(tokens):
   return expr()
 
 
-e=[VALUE(3), POWER(), VALUE(1), POWER(), VALUE(2), END()]
+#e=[VALUE(3), POWER(), VALUE(1), POWER(), VALUE(2), END()]
+# e = [MINUS(), VALUE(1), PLUS(), VALUE(2), MINUS(), VALUE(3), END()]
+e = [VALUE(1), PLUS(), VALUE(2), MINUS(), VALUE(3), END()]
 print(parse(e))
