b43259d201858c0f96a5bd6a40c8495901eb2dce
Alexandre Kandalintsev <spam@messir.net>
2013-09-23 00:01:32 +0200

    proper names for modules

--- ast.py
+++ ast.py
@@ -1,125 +1,100 @@
 #!/usr/bin/env python3
 
-"""
-Pratt-like parser.
-Inspired by http://effbot.org/zone/simple-top-down-parsing.htm
-"""
+from ast import prefix, infix, infix_r, postfix
 
-from io import StringIO
-import sys
+#####################
+# SOME OP TEMPLATES #
+#####################
 
-symap = {}
+class Binary:
+  def __init__(self, left, right):
+    self.left = left
+    self.right = right
 
-def symbol(sym, lbp=0):
-  try:
-    Sym = symap[sym]
-  except KeyError:
-    class Sym: pass
-    Sym.__name__ = 'Sym(%s)' % sym
-    Sym.__repr__ = lambda _: 'Sym(%s)' % sym
-    Sym.sym = sym
-    Sym.lbp = lbp
-    symap[sym] = Sym
-  else:
-    Sym.lbp = max(lbp, Sym.lbp)
-  return Sym
-
-
-def method(s):
-    # decorator
-    assert issubclass(s, symbol_base)
-    def bind(fn):
-        setattr(s, fn.__name__, fn)
-    return bind
-
-class prefix:
-  def __init__(self, sym, rbp):
-    self.sym = sym
-    self.rbp = rbp
-
-  def __call__(self, cls):
-    rbp = self.rbp
-    def nud(self):
-      return cls(expr(rbp))
-    symbol(self.sym).nud = nud
-    return cls
-
-
-class infix:
-  def __init__(self, sym, lbp):
-    self.sym = sym
-    self.lbp = lbp
-
-  def __call__(self, cls):
-    def led(self, left):
-      return cls(left, expr(self.lbp))
-    symbol(self.sym, self.lbp).led = led
-    return cls
-
-
-class infix_r:
-  def __init__(self, sym, lbp):
-    self.sym = sym
-    self.lbp = lbp
-
-  def __call__(self, cls):
-    def led(self, left):
-      return cls(left, expr(self.lbp-1))
-    symbol(self.sym, self.lbp).led = led
-    return cls
-
-
-class postfix:
-  def __init__(self, sym, lbp):
-    self.sym = sym
-    self.lbp = lbp
-
-  def __call__(self, cls):
-    def led(self, left):
-      return cls(left)
-    symbol(self.sym, self.lbp).led = led
-    return cls
-
-
-class Value:
-  lbp = 0
-  def __init__(self, value):
-    self.value = value
-  def nud(self):
-    return self
   def __repr__(self):
     cls = self.__class__.__name__
-    # return "(%s %s)" % (cls, self.value)
-    return "%s" % (self.value)
-  def codegen(self):
-    return self.value
+    return "(%s %s %s)" % (cls, self.left, self.right)
 
-class END:
-  lbp = 0
+class Unary:
+  def __init__(self, value):
+    self.value = value
   def __repr__(self):
-    return "END"
+    cls = self.__class__.__name__
+    return "(%s %s)" % (cls, self.value)
 
 
-###################
-# PRATT MACHINERY #
-###################
+#########
+# UNARY #
+#########
 
-def shift():
-  global nxt, e
-  return nxt, next(e)
+@prefix('-', 100)
+class Minus(Unary):
+  def type(self):
+    return self.value.type()
 
-def expr(rbp=0):
-  global cur, nxt
-  cur, nxt = shift()
-  left = cur.nud()
-  while rbp < nxt.lbp:
-    cur, nxt = shift()
-    left = cur.led(left)
-  return left
+@prefix('+', 100)
+class Plus(Unary):
+  def codegen(self):
+    # left = self.left
+    # ltype = left.type()
+    return self.value.codegen()
 
-def parse(tokens):
-  global cur, nxt, e
-  cur = nxt = None
-  e = tokens
-  cur, nxt = shift()
-  return expr()
+@prefix('p', 0)
+class Print(Unary):
+  def codegen(self):
+    return "printf(%s);" % self.value.codegen()
+
+@prefix('->', 2)
+class Lambda0(Unary):
+  def codegen(self):
+    return self.value.codegen()
+
+@postfix('!', 3)
+class CALL(Unary):
+  pass
+
+
+##########
+# BINARY #
+##########
+
+@infix('+', 10)
+class Add(Binary):
+  def codegen(self):
+    return "add(%s, %s)" % (self.left.codegen(), self.right.codegen())
+
+
+@infix('-', 10)
+class Sub(Binary):
+  def codegen(self):
+    return "sub(%s, %s)" % self.left.codegen(), self.right.codegen()
+
+
+@infix_r('^', 30)
+class Pow(Binary):
+  pass
+
+@infix_r('=', 1)
+class Eq(Binary):
+  def codegen(self):
+    if isinstance(self.right, (Lambda, Lambda0)):
+      r  = "void %s(void)\n" % self.left
+      r += "{%s}" % self.right.codegen()
+      return r
+    else:
+      return "%s = %s" % (self.left.codegen(), self.right.codegen())
+
+@infix('->', 2)
+class Lambda(Binary):
+  pass
+
+preamble = \
+"""
+#include <stdio.h>
+int add(int a, int b) {
+  return a+b;}
+
+int sub(int a, int b) {
+  return a-b;}
+
+"""
---
+++ pratt.py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python3
+
+"""
+Pratt-like parser.
+Inspired by http://effbot.org/zone/simple-top-down-parsing.htm
+"""
+
+from io import StringIO
+import sys
+
+symap = {}
+
+def symbol(sym, lbp=0):
+  try:
+    Sym = symap[sym]
+  except KeyError:
+    class Sym: pass
+    Sym.__name__ = 'Sym(%s)' % sym
+    Sym.__repr__ = lambda _: 'Sym(%s)' % sym
+    Sym.sym = sym
+    Sym.lbp = lbp
+    symap[sym] = Sym
+  else:
+    Sym.lbp = max(lbp, Sym.lbp)
+  return Sym
+
+
+def method(s):
+    # decorator
+    assert issubclass(s, symbol_base)
+    def bind(fn):
+        setattr(s, fn.__name__, fn)
+    return bind
+
+class prefix:
+  def __init__(self, sym, rbp):
+    self.sym = sym
+    self.rbp = rbp
+
+  def __call__(self, cls):
+    rbp = self.rbp
+    def nud(self):
+      return cls(expr(rbp))
+    symbol(self.sym).nud = nud
+    return cls
+
+
+class infix:
+  def __init__(self, sym, lbp):
+    self.sym = sym
+    self.lbp = lbp
+
+  def __call__(self, cls):
+    def led(self, left):
+      return cls(left, expr(self.lbp))
+    symbol(self.sym, self.lbp).led = led
+    return cls
+
+
+class infix_r:
+  def __init__(self, sym, lbp):
+    self.sym = sym
+    self.lbp = lbp
+
+  def __call__(self, cls):
+    def led(self, left):
+      return cls(left, expr(self.lbp-1))
+    symbol(self.sym, self.lbp).led = led
+    return cls
+
+
+class postfix:
+  def __init__(self, sym, lbp):
+    self.sym = sym
+    self.lbp = lbp
+
+  def __call__(self, cls):
+    def led(self, left):
+      return cls(left)
+    symbol(self.sym, self.lbp).led = led
+    return cls
+
+
+class Value:
+  lbp = 0
+  def __init__(self, value):
+    self.value = value
+  def nud(self):
+    return self
+  def __repr__(self):
+    cls = self.__class__.__name__
+    # return "(%s %s)" % (cls, self.value)
+    return "%s" % (self.value)
+  def codegen(self):
+    return self.value
+
+class END:
+  lbp = 0
+  def __repr__(self):
+    return "END"
+
+
+###################
+# PRATT MACHINERY #
+###################
+
+def shift():
+  global nxt, e
+  return nxt, next(e)
+
+def expr(rbp=0):
+  global cur, nxt
+  cur, nxt = shift()
+  left = cur.nud()
+  while rbp < nxt.lbp:
+    cur, nxt = shift()
+    left = cur.led(left)
+  return left
+
+def parse(tokens):
+  global cur, nxt, e
+  cur = nxt = None
+  e = tokens
+  cur, nxt = shift()
+  return expr()
