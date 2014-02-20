98b327a71a87dd3fe20ba7416b8491523a5911d7
Alexandre Kandalintsev <spam@messir.net>
2014-02-20 09:53:16 +0100

    dead.py: more cmd options

--- dead.py
+++ dead.py
@@ -1,22 +1,25 @@
 #!/usr/bin/env python3
 
 from log import logfilter
-from ast import parse
+from ast import parse, pretty_print
 from tokenizer import tokenize
+from interpreter import run
 import argparse
-
-
-def main():
-  parser = argparse.ArgumentParser()
-  # parser.add_argument('--show', action='store_const', const=True, default=True, help="show generated code")
-  parser.add_argument('input', nargs='+')
-  args = parser.parse_args()
-
-  for fname in args.input:
-    with open(fname) as fd:
-      src = fd.read()
-      tokens = tokenize(src)
-      ast = parse(tokens)
+from sys import exit
 
 if __name__ == '__main__':
-  main()
+  parser = argparse.ArgumentParser()
+  parser.add_argument('-t', '--tokens', action='store_const', const=True, default=False, help="show tokens")
+  parser.add_argument('-a', '--ast', action='store_const', const=True, default=False, help="show abstract syntax tree")
+  parser.add_argument('input', help="path to file")
+  args = parser.parse_args()
+
+  with open(args.input) as fd:
+    src = fd.read()
+    tokens = tokenize(src)
+    if args.tokens:
+      print(tokens)
+    ast = parse(tokens)
+    if args.ast:
+      pretty_print(ast)
+    exit(run(ast))
